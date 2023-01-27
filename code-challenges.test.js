c8fba490f2a17aadeb5e0628890ee1f38a81cf5b
SammyT13 <sammyt1316@yahoo.com>
2023-01-27 07:30:32 -0800

    completed week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,6 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -9,10 +10,28 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
-
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+
+// Test 1:
+
+describe('stringCoder', () => {
+    it('takes in a string and returns a string with a coded message', () => {
+        expect(stringCoder(secretCodeWord1)).toEqual('L4ck4d41s1c4l')
+        expect(stringCoder(secretCodeWord2)).toEqual('G0bbl3dyg00k')
+        expect(stringCoder(secretCodeWord3)).toEqual('3cc3ntr1c')
+    })
+})
+
+// *** Good Test Failure *** //
+// FAIL  ./code-challenges.test.js
+// stringCoder
+//   ✕ takes in a string and returns a string with a coded message (1 ms)
+
+// ● stringCoder › takes in a string and returns a string with a coded message
+
+//   ReferenceError: stringCoder is not defined
 
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
@@ -21,11 +40,65 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+
 // b) Create the function that makes the test pass.
+
+// Psuedocode:
+// Function Signature
+// input: string
+// output: string
+
+// input: const secretCodeWord1 = "Lackadaisical"
+// output: "L4ck4d41s1c4l"
+
+// input: const secretCodeWord2 = "Gobbledygook"
+// output: "G0bbl3dyg00k"
+
+// input: const secretCodeWord3 = "Eccentric"
+// output: "3cc3ntr1c"
+
+// Strategy: Create a function called stringCoder that takes a string as a parameter
+// In the Function: create an empty variable
+// Use the split method to create an array
+// Use the map method to map through the array (only need i for index)
+// Use ternary operator to determine the sortedArr of each index and change accordingly
+// return encodedWord
+
+const stringCoder = (string) => {
+    let encodedWord = ''
+    string.split('').map((_,i) => {
+       encodedWord = string[i].toLowerCase() === 'a' ? encodedWord += '4' : string[i].toLowerCase() === 'e' ? encodedWord += '3' : string[i].toLowerCase() === 'i' ? encodedWord += '1' : string[i].toLowerCase() === 'o' ? encodedWord += '0' : encodedWord += string[i]
+    })
+    return encodedWord
+}
+
+// *** Test Passed *** //
+// PASS  ./code-challenges.test.js
+// stringCoder
+//   ✓ takes in a string and returns a string with a coded message (10 ms)
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+
+// Test 2:
+
+describe('wordsWithChar', () => {
+    it('takes in an array of words and a single letter and returns an array of all the words containing that particular letter', () => {
+        expect(wordsWithChar(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(wordsWithChar(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
+
+// *** Good Test Failure *** //
+// FAIL  ./code-challenges.test.js
+// wordsWithChar
+// ✕ takes in an array of words and a single letter and returns an array of all the words containing that particular letter (1 ms)
+
+// ● wordsWithChar › takes in an array of words and a single letter and returns an array of all the words containing that particular letter
+
+// ReferenceError: wordsWithChar is not defined
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -36,9 +109,56 @@ const letterE = "e"
 
 // b) Create the function that makes the test pass.
 
+// Psuedocode
+// Function Signature
+// input: array of strings and a character
+// output: array of strings
+
+// input: const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]  and const letterA = "a"
+// output: ["Mango", "Apricot", "Peach"]
+
+// input: const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"] and const letterE = "e"
+// output: ["Cherry", "Blueberry", "Peach"]
+
+// Strategy: create a function called wordsWithChar that will take in two parameters
+// Parameters:  an array of strings and a character
+// Inside the function: 
+// use the filter method to compare each each word in the array with character
+// return array containing words that match character
+
+const wordsWithChar = (array,char) => {
+    return array.filter((word) => {
+        return word.includes(char) || word.includes(char.toUpperCase())
+    })
+}
+
+// *** Test Passed *** //
+//PASS  ./code-challenges.test.js
+// wordsWithChar
+// ✓ takes in an array of words and a single letter and returns an array of all the words containing that particular letter (1 ms)
+
+
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+describe('fullHouse', () => {
+    it('takes in an array of 5 numbers and determines whether or not the array is a full house', () => {
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
+
+// *** Good Test Failure *** //
+// FAIL  ./code-challenges.test.js
+// fullHouse
+// ✕ takes in an array of 5 numbers and determines whether or not the array is a full house (3 ms)
+
+// ● fullHouse › takes in an array of 5 numbers and determines whether or not the array is a full house
+
+// ReferenceError: fullHouse is not defined
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -50,3 +170,45 @@ const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
 // b) Create the function that makes the test pass.
+
+// Psuedocode
+// Function signature
+// input: an array of numbers
+// output: boolean
+
+// input: const hand1 = [5, 5, 5, 3, 3]
+// output: true
+
+// input:const hand2 = [5, 5, 3, 3, 4]
+// output: false
+
+// input: const hand3 = [5, 5, 5, 5, 4]
+// output: false
+
+// input: const hand4 = [7, 2, 7, 2, 7]
+// output: true
+
+// Strategy: create a function called fullHouse that will take an array of numbers as a parameter
+// In the Function: 
+// sort the arrays in ascending order (this will group the numbers from small to large)
+// ternary operator to check for last three numbers in array are the same
+// ternary operator to check for the first two numbers in array are the same
+// return true or false (depends if threeOfAKind and twoOfAkind are both true or one is false)
+// *** SIDE NOTE ***
+// This function does not account if three of kind are smaller than the two of the kind
+// This is something I can work on, but at the moment I just couldn't figure it out
+// Will return to refactor function to account for additional edge cases
+
+const fullHouse = (handArray) => {
+    handArray.sort((a,b) => a - b)
+
+    let threeOfAKind = handArray[2] === handArray[3] && handArray[2] === handArray[4] ? true : false
+    let twoOfAKind = handArray[0] === handArray[1] ? true : false
+    
+    return threeOfAKind && twoOfAKind
+}
+
+// *** Test Passed *** //
+// PASS  ./code-challenges.test.js
+// fullHouse
+// ✓ takes in an array of 5 numbers and determines whether or not the array is a full house (5 ms)
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -8,14 +8,43 @@ beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
-# Expected output: ['tea', 'water', 'soda water']
+# Expected output: ['tea', 'water', 'soda water'] 
 
+# Psuedocode
+# create a method that takes in an array and character
+# use the select method to return a new array
+# use the do method to pass each word in the array
+# use the include method to see if any word in the array contains a specified character
+
+def word_char_locator (array, char)
+    array.select do |word|
+        word.include?(char)
+    end
+end
+
+p word_char_locator(beverages_array, letter_o)
+# Output: ["coffee", "soda water"]
+p word_char_locator(beverages_array, letter_t)
+# Output: ["tea", "water", "soda water"]
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
+
+# Psuedocode
+# create a method that takes in a hash
+# use the values method to check the values in the hash
+# use the flatten methed to remove nested arrays and have one array
+# use the sort method to sort states in alphabetical order
+
+def hash_state_sorter states
+    states.values.flatten.sort
+end
+
+p hash_state_sorter(us_states)
+# Output: ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"]
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
@@ -30,3 +59,59 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+# Psuedocode
+# Create a class name Bike
+# Use method to to set model, wheels, and current speed
+# Use initialize method for model, wheels, and current speed
+# Create a method with bike information (model, wheels, current speed)
+# Create a method to increase speed of bike
+# Create a method to decrease speed of bike
+    #  Use teranary operator to determine if the speed is at 0 or below to prevent speed from going negative
+
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+
+    def initialize model
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+    def pedal_faster speed
+        @current_speed += speed
+    end
+    def brake speed
+        @current_speed -= speed
+        @current_speed <= 0 ? @current_speed = 0 : @current_speed
+    end
+
+end
+
+#-----3a Answers:
+my_bike = Bike.new('Trek')
+p my_bike.bike_info
+# Output: "The Trek bike has 2 wheels and is going 0 mph."
+
+my_bike_2 = Bike.new('Schwinn')
+p my_bike_2.bike_info
+# Output: "The Schwinn bike has 2 wheels and is going 0 mph."
+
+#-----3b Answers:
+my_bike.pedal_faster(10)
+p my_bike.bike_info
+# Output: "The Trek bike has 2 wheels and is going 0 mph."
+
+my_bike.pedal_faster(18)
+p my_bike.bike_info
+# Output: "The Trek bike has 2 wheels and is going 10 mph."
+
+my_bike.brake(5)
+p my_bike.bike_info
+# Output: "The Trek bike has 2 wheels and is going 28 mph."
+
+my_bike.brake(25)
+p my_bike.bike_info
+# Output: "The Trek bike has 2 wheels and is going 23 mph."
